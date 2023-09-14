efd3ad38bd316184d31ab47731226613f5621855
gayathrivelu10 <93238860+gayathrivelu10@users.noreply.github.com>
2023-09-14 20:04:03 +0530

    z added

--- first.java
+++ first.java
@@ -1,6 +1,6 @@
 public class demo(){
 public static void main(String args[]){
- int x=12,y=34;
- int result=x+y;
+ int x=12,y=34,z=67;
+ int result=x+y+z;
  System.out.println("Result:"+result);
 }}
